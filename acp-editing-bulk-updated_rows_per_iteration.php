e943ae8ff129946b81a453b2da86df2747487c88
Tobias Schutter <tschutter@gmail.com>
2020-08-05 17:02:43 +0200

    Default comment

--- acp-editing-bulk-updated_rows_per_iteration.php
+++ acp-editing-bulk-updated_rows_per_iteration.php
@@ -2,16 +2,15 @@
 /**
  * Change the amount of items being prepared per batch process for Bulk Edit.
  * This process is done before the actual processing finds place.
- * Default amount is 2000
  */
 
 /**
  * Prepare less items if you experiencing performance issues or time out issues
  *
- * @param int $amount
+ * @param int $amount Default is `2000`
  */
 function acp_editing_bulk_less_items_per_iteration( $amount ) {
-	return 100;
+	return 100; // Default is 2000
 }
 
 add_filter( 'acp/editing/bulk/editable_rows_per_iteration', 'acp_editing_bulk_less_items_per_iteration' );
