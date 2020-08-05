f1989971111cbfac1c38b38053023044da5b035f
Tobias Schutter <tschutter@gmail.com>
2020-08-05 16:05:47 +0200

    Column value

--- ac-column-value.php
+++ ac-column-value.php
@@ -4,7 +4,18 @@
  * You probably want to check for a specific column instance and check for extra conditionals related to the column in order to change the value for the correct column
  */
 
+/**
+ * @param string    $value
+ * @param int       $id
+ * @param AC\Column $column
+ *
+ * @return string
+ */
 function ac_column_value_usage( $value, $id, AC\Column $column ) {
+
+	// Change the rendered column value
+	// $value = 'new value';
+
 	return $value;
 }
 
@@ -13,9 +24,9 @@ add_filter( 'ac/column/value', 'ac_column_value_usage', 10, 3 );
 /**
  * Example on how to alter the value based on an ACF Column. It defines different variables that can be used to check for specific conditionals
  *
- * @param            $value
- * @param            $id
- * @param \AC\Column $column
+ * @param string    $value
+ * @param int       $id
+ * @param AC\Column $column
  *
  * @return string
  */
@@ -39,19 +50,23 @@ add_filter( 'ac/column/value', 'ac_column_value_acf_example', 10, 3 );
 /**
  * Example on how to wrap the value of a specific Custom Field column of the type 'color' in markup to give it a background color
  *
- * @param            $value
- * @param            $id
- * @param \AC\Column $column
+ * @param string    $value
+ * @param int       $id
+ * @param AC\Column $column
  *
  * @return string
  */
 function ac_column_value_custom_field_example( $value, $id, AC\Column $column ) {
 	if ( $column instanceof ACP\Column\CustomField ) {
-		$meta_key = $column->get_meta_key();
-		$type = $column->get_field_type(); // Gets the Custom Field Type key ( '' = default, 'text','textarea','color','date','numeric','image','link','checkmark', 'library_id', 'title_by_id', 'user_by_id'
 
-		if ( 'my_hex_color' === $meta_key && 'color' !== $type ) {
-			$value = sprintf( '<span style="color: %1$s">%1$s</span>', $value );
+		// Custom Field Key
+		$meta_key = $column->get_meta_key();
+
+		// Custom Field Type can be 'text|textarea|color|date|numeric|image|link|checkmark|library_id|title_by_id|user_by_id'. The default is ''.
+		$custom_field_type = $column->get_field_type();
+
+		if ( 'my_hex_color' === $meta_key && 'color' !== $custom_field_type ) {
+			$value = sprintf( '<span style="background-color: %1$s">%1$s</span>', $value );
 		}
 	}
 
@@ -61,11 +76,40 @@ function ac_column_value_custom_field_example( $value, $id, AC\Column $column ) 
 add_filter( 'ac/column/value', 'ac_column_value_custom_field_example', 10, 3 );
 
 /**
+ * Example on how to add a `class` attribute to the rendered value that can be styled by CSS.
+ *
+ * @param string    $value
+ * @param int       $id
+ * @param AC\Column $column
+ *
+ * @return string
+ */
+function ac_column_value_add_class_attribute_based_on_value( $value, $id, AC\Column $column ) {
+	if ( $column instanceof ACP\Column\CustomField ) {
+
+		// Add a unqiue `class` attribute to the rendered value.
+
+		if ( 'my_custom_field_key' === $column->get_meta_key() ) {
+			$value = sprintf(
+				'<span class="%s %s">%s</span>',
+				esc_attr( 'column-' . $column->get_name() ), // Add the column name to the `class` attribute
+				esc_attr( 'value-' . sanitize_key( $value ) ),  // Add a sanitized column value to the `class` attribute
+				$value
+			);
+		}
+	}
+
+	return $value;
+}
+
+add_filter( 'ac/column/value', 'ac_column_value_add_class_attribute_based_on_value', 10, 3 );
+
+/**
  * Example to target the taxonomy column and prefix it with the taxonomy name
  *
- * @param            $value
- * @param            $id
- * @param \AC\Column $column
+ * @param string    $value
+ * @param int       $id
+ * @param AC\Column $column
  *
  * @return string
  */
