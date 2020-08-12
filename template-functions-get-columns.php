5ccdff1673b124aab017c8b8813c7d6d4bc9c283
Tobias Schutter <tschutter@gmail.com>
2020-08-12 13:37:03 +0200

    API methods

---
+++ template-functions-get-columns.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Fetch all columns for a particular list table.
+ */
+function acp_loaded() {
+
+	/**
+	 * The "List Screen ID" can be found by opening the screen options in the top-right corner of the
+	 * admin columns settings page. Enable the "List screen ID" by clicking the checkbox next to it.
+	 * The list screen ID will now be visible in the right sidebar.
+	 * @var string $list_screen_id
+	 */
+	$list_screen_id = "<LIST SCREEN ID GOES HERE>"; // e.g. '5e84e8302794f'
+
+	/**
+	 * @var AC\ListScreen $list_screen
+	 */
+	$list_screen = ac_get_list_screen( $list_screen_id );
+
+	/**
+	 * Get all column objects
+	 * @var AC\Column[] $columns
+	 */
+	$columns = $list_screen->get_columns();
+
+}
+
+add_action( 'wp_loaded', 'acp_loaded' );
---
+++ template-functions-get-list-screens.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Fetch all list screens for a particular list table.
+ */
+function acp_loaded() {
+
+	/**
+	 * The "List Screen Key" can be found by opening the screen options in the top-right corner of the
+	 * admin columns settings page. Enable the "List screen Key" by clicking the checkbox next to it.
+	 * The list screen Key will now be visible in the right sidebar.
+	 * @var string $list_screen_key
+	 */
+	$list_screen_key = "<LIST SCREEN KEY GOES HERE>"; // e.g. page
+
+	/**
+	 * @var AC\ListScreen[] $list_screens
+	 */
+	$list_screens = ac_get_list_screens( $list_screen_key );
+}
+
+add_action( 'wp_loaded', 'acp_loaded' );
