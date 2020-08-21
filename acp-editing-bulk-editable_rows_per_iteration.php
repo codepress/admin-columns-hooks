ae1e96a88e3eb6cde1d99e8b37be1b5876d90266
Tobias Schutter <tschutter@gmail.com>
2020-08-21 17:20:58 +0200

    Doc block

--- acp-editing-bulk-editable_rows_per_iteration.php
+++ acp-editing-bulk-editable_rows_per_iteration.php
@@ -8,12 +8,12 @@
  * Reduce the amount of items that is saved per batch operation.
  * Lower this when experiencing time out issues
  *
- * @param int $number
+ * @param int $number Default is 250.
  *
  * @return int
  */
 function acp_editing_bulk_reduce_records_to_update_per_iteration( $number ) {
-	return 10;
+	return 10; // Default is 250
 }
 
 add_filter( 'acp/editing/bulk/updated_rows_per_iteration', 'acp_editing_bulk_reduce_records_to_update_per_iteration' );
