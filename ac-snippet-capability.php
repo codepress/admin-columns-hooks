26287999dc502a31a5392b6aa75cfee84d24db61
Tobias Schutter <tschutter@gmail.com>
2020-08-31 18:34:30 +0200

    Code snippet

---
+++ ac-snippet-capability.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Allow another role to manage columns.
+ * By default, only administrator are allowed to manage columns.
+ */
+add_action( 'admin_init', function () {
+	global $wp_roles;
+
+	// Allow users with the role 'editor' to manage columns
+	$wp_roles->add_cap( 'editor', AC\Capabilities::MANAGE );
+} );
