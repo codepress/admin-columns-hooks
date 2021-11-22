41ddd9c6866486f65b268becf324a59e4b415b1e
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2021-11-22 11:02:17 +0100

    Create acp-sorting-custom_field-numeric_type.php

---
+++ acp-sorting-custom_field-numeric_type.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This filter allows you to set the data type of a custom `numeric` field used to sort the column.
+ */
+
+/**
+ * Usage of the hook
+ *
+ * @param string                $date_type 'numeric' or 'decimal'.
+ * @param AC\Column\CustomField $column
+ *
+ * @return string
+ */
+function acp_sorting_custom_field_numeric_type( $date_type, AC\Column\CustomField $column ) {
+
+	// Change the custom field type
+	// $data_type = 'numeric';
+
+	return $date_type;
+}
+
+add_filter( 'acp/sorting/custom_field/date_type', 'acp_sorting_custom_field_numeric_type', 10, 2 );
+
+/**
+ * Set the sorting data type for a specific custom field to decimal
+ *
+ * @param string                $date_type 'numeric' or 'decimal'.
+ * @param AC\Column\CustomField $column
+ *
+ * @return string
+ */
+function acp_sorting_use_decimal_data_type_for_specific_field( $data_type, AC\Column\CustomField $column ) {
+	if ( 'you_meta_key' === $column->get_meta_key() ) {
+		$data_type = 'decimal';
+	}
+
+	return $data_type;
+}
+
+add_filter( 'acp/sorting/custom_field/numeric_type', 'acp_sorting_use_decimal_data_type_for_specific_field', 10, 2 );
+
+/**
+ * Set the sorting data type for a specific custom field
+ * @return string
+ */
+function acp_sorting_use_decimal_data_type() {
+	return 'decimal';
+}
+
+add_filter( 'acp/sorting/custom_field/numeric_type', 'acp_sorting_use_decimal_data_type', 10, 2 );
