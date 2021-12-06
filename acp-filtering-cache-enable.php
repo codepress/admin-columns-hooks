2c6eea1f99a4fa67e36aad1f0b30da1efae4d431
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2021-12-06 09:07:07 +0100

    Create acp-filtering-cache-enable.php

---
+++ acp-filtering-cache-enable.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This filter allows you to disable cache for the filtering feature
+ */
+
+/**
+ * Disable caching for the filtering
+ */
+add_filter( 'acp/filtering/cache/enable', '__return_false' );
+
+/**
+ * Disable caching for the custom field column
+ *
+ * @param bool      $enabled
+ * @param AC\Column $column
+ *
+ * @return bool
+ */
+function acp_filtering_disable_cache_for_custom_field_column( $enabled, AC\Column $column ) {
+	return $column instanceof AC\Column\CustomField
+		? false
+		: $enabled;
+}
+
+add_filter( 'acp/filtering/cache/enable', 'acp_filtering_disable_cache_for_custom_field_column' );
