3a3d74961e8fa04e0480e033c9f816db1b52b02a
Tobias Schutter <tobias@codepress.nl>
2022-01-24 15:05:31 +0100

    Added examples for export filename

--- acp-export-filename.php
+++ acp-export-filename.php
@@ -7,34 +7,87 @@
 /**
  * Usage of the export filename hook
  *
- * @param string        $fileName
- * @param AC\ListScreen $listScreen
+ * @param string $file_name
+ * @param AC\ListScreen $list_screen
  *
  * @return string
  */
-function acp_export_filename_usage( $fileName, AC\ListScreen $listScreen ) {
+function acp_export_filename_usage( $file_name, AC\ListScreen $list_screen ) {
 
-	// Replace fileName or use ListScreen to be used in the hook
+	// Replace $file_name or use $list_screen to be used in the hook
 
-	return $fileName;
+	return $file_name;
 }
 
 add_filter( 'acp/export/file_name', 'acp_export_filename_usage', 10, 2 );
 
+/**
+ * Example of a custom filename structure for every list screen
+ *
+ * @param string $file_name
+ * @param AC\ListScreen $list_screen
+ *
+ * @return string
+ */
+function acp_export_filename_custom_structure( $file_name, AC\ListScreen $list_screen ) {
 
+	// Target a specific post type
+	$post_type = 'my_custom_post_type';
+
+	if ( $list_screen instanceof AC\ListScreenPost && $post_type === $list_screen->get_post_type() ) {
+
+		// Replace the filename with a custom label
+		// e.g. 'My Custom Label.csv'
+		$file_name = 'My Custom Label';
+	}
+
+	return $file_name;
+}
+
+add_filter( 'acp/export/file_name', 'acp_export_filename_custom_structure', 10, 2 );
 
 /**
  * Example of a custom filename structure for every list screen
  *
- * @param string        $fileName
- * @param AC\ListScreen $listScreen
+ * @param string $file_name
+ * @param AC\ListScreen $list_screen
  *
  * @return string
  */
-function acp_export_filename_custom_structure( $fileName, AC\ListScreen $listScreen ) {
-	$fileName = $listScreen->get_id()->get_id() . $listScreen->get_key() . date('Y-m-d' );
+function acp_export_filename_user_structure( $file_name, AC\ListScreen $list_screen ) {
 
-	return $fileName;
+	// Replace the filename with your username
+	// e.g. 'Oliver-Hardy-Cars.csv'
+	$file_name = sprintf(
+		'%s-%s-%s',
+		wp_get_current_user()->first_name,
+		wp_get_current_user()->last_name,
+		$list_screen->get_label()
+	);
+
+	return $file_name;
 }
 
-add_filter( 'acp/export/file_name', 'acp_export_filename_custom_structure', 10, 2 );
+add_filter( 'acp/export/file_name', 'acp_export_filename_user_structure', 10, 2 );
+
+/**
+ * Example of a custom filename structure for every list screen
+ *
+ * @param string $file_name
+ * @param AC\ListScreen $list_screen
+ *
+ * @return string
+ */
+function acp_export_filename_date_structure( $file_name, AC\ListScreen $list_screen ) {
+
+	// Replace the filename with a custom label and custom date including a timestamp
+	// e.g. 'Pages on 2022 March 10th 17:16.csv'
+	$file_name = sprintf(
+		'%s on %s',
+		$list_screen->get_label(), date_i18n( "Y F jS H:i" )
+	);
+
+	return $file_name;
+}
+
+add_filter( 'acp/export/file_name', 'acp_export_filename_date_structure', 10, 2 );
