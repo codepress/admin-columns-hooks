072507519b1ead8a3a47a47503263ed3ebff4132
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2022-02-07 12:46:57 +0100

    Editing View hook

---
+++ acp-editing-view.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Change the behavior of the editable for a specific column.
+ */
+
+/**
+ * Usage
+ *
+ * @param ACP\Editing\View|false $view
+ * @param AC\Column              $column
+ * @param string                 $context "single" or "bulk"
+ * @param ACP\Editing\Service    $service
+ *
+ * @return ACP\Editing\View|false
+ */
+function acp_editing_view_example_usage( $view, AC\Column $column, $context, ACP\Editing\Service $service ) {
+	return $view;
+}
+
+add_filter( 'acp/editing/view', 'acp_editing_view_example_usage', 10, 4 );
+
+// Or anonymous function
+
+add_filter( 'acp/editing/view', function ( $view, AC\Column $column, $context, ACP\Editing\Service $service ) {
+	return $view;
+}, 10, 4 );
+
+/**
+ * Example that enabled bulk editing for the Slug column that is disabled by default. It returns the view that is normally used for inline editing
+ *
+ * @param ACP\Editing\View|false $view
+ * @param AC\Column              $column
+ * @param string                 $context "single" or "bulk"
+ * @param ACP\Editing\Service    $service
+ *
+ * @return ACP\Editing\View|false
+ */
+function acp_editing_view_enable_bulk_for_slug( $view, AC\Column $column, $context, ACP\Editing\Service $service ) {
+	if( $column instanceof AC\Column\Post\Slug && 'bulk' === $context ){
+		$view = $service->get_view( 'single' );
+	}
+
+	return $view;
+}
+
+add_filter( 'acp/editing/view', 'acp_editing_view_enable_bulk_for_slug', 10, 4 );
