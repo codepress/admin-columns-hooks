3aad91a9e06879e487b858daeab5f1eb0a08e1f5
Tobias Schutter <tobias@codepress.nl>
2022-04-29 11:56:40 +0200

    Create ac-column-separator.php

---
+++ ac-column-separator.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The filter `ac/column/separator` allows you to alter the separator value of a column cell
+ * You probably want to check for a specific column instance and check for extra conditionals related to the column in order to change the value for the correct column
+ */
+
+/**
+ * @param string $separator The column separator value
+ * @param AC\Column $column Column object
+ *
+ * @return string
+ */
+function ac_column_separator( $separator, AC\Column $column ) {
+
+	// Change the rendered column separator value
+	// $separator = ',';
+
+	return $separator;
+}
+
+add_filter( 'ac/column/separator', 'ac_column_separator', 10, 3 );
+
+// Changes the separator value from a comma (,) to a newline (<br>) for an ACF relationship column
+add_filter( 'ac/column/separator', function ( $separator, $column ) {
+	if ( $column instanceof \ACA\ACF\Column && $column->get_field() instanceof \ACA\ACF\Field\Type\Relationship ) {
+		$separator = "<br>";
+	}
+
+	return $separator;
+}, 10, 2 );
