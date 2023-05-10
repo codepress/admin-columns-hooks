9f7e8b394f39648ee3dd6449904718b4e9ddc616
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2023-05-10 15:05:33 +0200

    New hooks for version 6.3

---
+++ ac-list_screen-is_active.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This hooks allows you to disable admin columns support for a specific list table
+ * This hook will not remove the support from the admin settings. You can use the hook 'ac/list_screen/key/is_active to remove the support on a more global level
+ */
+
+/** Usage Example: Short notation */
+add_filter( 'ac/list_screen/is_active', function ( bool $active, WP_Screen $screen ) {
+	return $active;
+}, 10, 2 );
+
+/** Disable Admin Columns on the list table for the Song Custom Post Tepe */
+add_filter( 'ac/list_screen/key/is_active', function ( $active, WP_Screen $screen ) {
+	if ( $screen->post_type && 'song' === $screen->post_type ) {
+		return false;
+	}
+
+	return $active;
+}, 10, 2 );
+
+/** Disable Admin Columns for all Editors */
+add_filter( 'ac/list_screen/key/is_active', function ( $active, WP_Screen $screen ) {
+	if ( current_user_can( 'editor' ) ) {
+		return false;
+	}
+
+	return $active;
+}, 10, 2 );
---
+++ ac-list_screen-key-is_active.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This hooks allows you to disable admin columns support for a specific list table based on the used key.
+ * It is most likely that the hook `ac/list_screen/is_active` must also be used to disable the table view
+ */
+
+/** Usage Example: Short notation */
+add_filter( 'ac/list_screen/key/is_active', function ( bool $active, string $key ) {
+	return $active;
+}, 10, 2 );
+
+/** Disable Admin Columns for the Media List Table */
+add_filter( 'ac/list_screen/key/is_active', function ( $active, $key ) {
+	if ( 'wp-media' === $key ) {
+		return false;
+	}
+
+	return $active;
+}, 10, 2 );
