1534be39f505cbd1ff55b0799dd2dd7da797d9ca
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2023-10-02 14:40:44 +0200

    Update acp-editing-value.php

--- acp-editing-value.php
+++ acp-editing-value.php
@@ -10,17 +10,18 @@
  *
  * @return mixed
  */
-function acp_editing_value_example_usage( $value, $id, AC\Column $column ) {
-	return $value;
+function acp_editing_value_example_usage($value, $id, AC\Column $column)
+{
+    return $value;
 }
 
-add_filter( 'acp/editing/value', 'acp_editing_value_example_usage', 10, 3 );
+add_filter('acp/editing/value', 'acp_editing_value_example_usage', 10, 3);
 
 // Or anonymous function
 
-add_filter( 'acp/editing/view', function ( $value, $id, AC\Column $column ) {
-	return $value;
-}, 10, 3 );
+add_filter('acp/editing/value', function ($value, $id, AC\Column $column) {
+    return $value;
+}, 10, 3);
 
 /**
  * Example to get a nested value from an associative array. You need to parse it back when saving the value with the hook `acp/editing/save_value`
@@ -31,12 +32,13 @@ add_filter( 'acp/editing/view', function ( $value, $id, AC\Column $column ) {
  *
  * @return mixed
  */
-function acp_editing_value_get_nested_value( $value, $id, AC\Column $column ) {
-	if ( $column instanceof AC\Column\Customfield && 'my_custom_field_key' === $column->get_meta_key() ) {
-		$value = $value['sub_key'];
-	}
+function acp_editing_value_get_nested_value($value, $id, AC\Column $column)
+{
+    if ($column instanceof AC\Column\Customfield && 'my_custom_field_key' === $column->get_meta_key()) {
+        $value = $value['sub_key'];
+    }
 
-	return $value;
+    return $value;
 }
 
-add_filter( 'acp/editing/value', 'acp_editing_value_get_nested_value', 10, 3 );
+add_filter('acp/editing/value', 'acp_editing_value_get_nested_value', 10, 3);
