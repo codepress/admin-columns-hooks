b308ed5f692ff6d9a592bea396a973a1b801fd02
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2024-06-11 16:15:09 +0200

    Update ac-export-value-escape.php

--- ac-export-value-escape.php
+++ ac-export-value-escape.php
@@ -1,6 +1,17 @@
 <?php
 /**
- *
+ * The following filter allows you to disable the default sanitazion of cell values.
+ * Because of the sanitazion value started with =,-,+, and @ are prefixed with a quote to prevent cell execution
  */
 
+// This example disabled sanitation for all columns
 add_filter('ac/export/value/escape', '__return_false');
+
+// This example disabled sanitation for all Custom Field Columns
+add_filter('ac/export/value/escape', static function ($enabled, Ac\Column $column) {
+    if ($column instanceof AC\Column\CustomField) {
+        return false;
+    }
+
+    return $enabled;
+}, 10, 2);
