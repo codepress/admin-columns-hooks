ca9957217bd125c72194ed915943b7ba22e7eb79
Tobias Schutter <tobias@codepress.nl>
2026-02-10 11:56:18 +0100

    renamed ac/column/types

--- ac-column-types.php
+++ ac-column-types.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * Register your custom column type in the 'acp/column_types' hook.
+ * A full working example is provided in the 'ac-column-template' plugin.
+ * @see https://github.com/codepress/ac-column-template/
  */
-add_filter('ac/column/types/pro', static function (array $factories, AC\TableScreen $table_screen): array {
+add_filter('ac/column/types', static function (array $factories, AC\TableScreen $table_screen): array {
     // Include file with the column class
     require_once __DIR__ . '/classes/MyExampleColumn.php';
 
     // Register for the page post type table
-    if ($table_screen->get_id()->equals(new AC\Type\TableId('page'))) {
+    if ('page' === (string)$table_screen->get_id()) {
         $factories[] = MyExampleColumn::class;
     }
 
@@ -20,9 +21,8 @@ add_filter('ac/column/types/pro', static function (array $factories, AC\TableScr
     // Register for the Taxonomy table
     if (
         $table_screen instanceof ACP\TableScreen\Taxonomy &&
-        $table_screen->get_taxonomy()->equals(
-            new AC\Type\TaxonomySlug('category')
-        )) {
+        'category' === (string)$table_screen->get_taxonomy()
+    ) {
         $factories[] = MyExampleColumn::class;
     }
 
